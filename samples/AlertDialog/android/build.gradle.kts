c7a71adcd5d54097805954300bf60919b505b325
atsushieno <atsushieno@gmail.com>
2021-11-21 01:15:15 +0900

    update targetSdkVersion to 31.

--- samples/AlertDialog/android/build.gradle.kts
+++ samples/AlertDialog/android/build.gradle.kts
@@ -13,11 +13,11 @@ dependencies {
 }
 
 android {
-    compileSdkVersion(30)
+    compileSdkVersion(31)
     defaultConfig {
         applicationId = "dev.atsushieno.android"
         minSdkVersion(24)
-        targetSdkVersion(30)
+        targetSdkVersion(31)
         versionCode = 1
         versionName = "1.0"
     }
